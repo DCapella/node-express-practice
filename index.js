da9bb6531a3261d4861be87394d41337ea511f42
David Capella <D_Capella@yahoo.com>
2018-03-28 13:42:45 -0500

    trying out logger

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const express = require('express');
+const logger = require('morgan');
 const http = require('http');
 const app = express();
 
+app.use(logger());
+
+// Homepage
 app.use((req, res, next) => {
-  console.log("in comes a " + req.method + " to " + req.url);
-  next();
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end("Welcome to the homepage!\n");
+  } else {
+    next();
+  }
 });
 
+// About
+app.use((req, res, next) => {
+  if (req.url === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end("Here is the about page!\n");
+  } else {
+    next();
+  }
+});
+
+// 404
 app.use((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World!\n");
+  res.writeHead(404, { 'Content-Type': 'text/plain' });
+  res.end("404 Error!\n");
 });
 
 http.createServer(app).listen(1337);
